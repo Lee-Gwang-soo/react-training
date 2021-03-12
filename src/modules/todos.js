470b84766bc277732da6d53e1de23431c4d2881f
Lee-Gwang-soo <59168432+Lee-Gwang-soo@users.noreply.github.com>
2021-03-12 14:55:42 +0900

    Refactoring redux => redux-toolkit

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,78 +1,124 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const CHANGE_INPUT = 'todos/CHANGE_INPUT';
-const INSERT = 'todos/INSERT';
-const UPDATE = 'todos/UPDATE';
-const TOGGLE = 'todos/TOGGLE';
-const REMOVE = 'todos/REMOVE';
+// const CHANGE_INPUT = 'todos/CHANGE_INPUT';
+// const INSERT = 'todos/INSERT';
+// const UPDATE = 'todos/UPDATE';
+// const TOGGLE = 'todos/TOGGLE';
+// const REMOVE = 'todos/REMOVE';
 
-export const changeInput = (key, value) => {
-	return {
-		type: CHANGE_INPUT,
-		key,
-		value,
-	};
-};
+// export const changeInput = (key, value) => {
+// 	return {
+// 		type: CHANGE_INPUT,
+// 		key,
+// 		value,
+// 	};
+// };
 
-// export const changeInput = (name, description) => ({
-// 	type: CHANGE_INPUT,
-// 	name,
-// 	description,
+// export const update = (name) => ({
+// 	type: UPDATE,
+// 	todo: {
+// 		name: name,
+// 	},
+// });
+// let id = 3;
+// export const insert = (name, description, createdAt, updatedAt) => ({
+// 	type: INSERT,
+// 	todo: {
+// 		id: id++,
+// 		name,
+// 		done: false,
+// 		description,
+// 		createdAt: current,
+// 		updatedAt: current,
+// 	},
 // });
 
-export const update = (name) => ({
-	type: UPDATE,
-	todo: {
-		name: name,
-	},
-});
-let id = 3;
-export const insert = (name, description, createdAt, updatedAt) => ({
-	type: INSERT,
-	todo: {
-		id: id++,
-		name,
-		done: false,
-		description,
-		createdAt: current,
-		updatedAt: current,
-	},
-});
+// export const toggle = (id) => ({
+// 	type: TOGGLE,
+// 	id,
+// });
 
-export const toggle = (id) => ({
-	type: TOGGLE,
-	id,
-});
-
-export const remove = (id) => ({
-	type: REMOVE,
-	id,
-});
+// export const remove = (id) => ({
+// 	type: REMOVE,
+// 	id,
+// });
 
 var now = new Date();
 var current = `${now.getFullYear()}/${now.getMonth()}/${now.getDate()}/ ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-const initialState = {
-	name: '',
-	description: '',
-	todos: [
-		{
-			id: 1,
-			name: '온보딩 만들기',
-			done: true,
-			createdAt: current,
-			updatedAt: current,
-			description: '온보딩을 만들어보자!',
+let id = 3;
+
+export const TodoSlice = createSlice({
+	name: 'Todolist',
+	initialState: {
+		name: '',
+		description: '',
+		todos: [
+			{
+				id: 1,
+				name: '온보딩 만들기',
+				done: true,
+				createdAt: current,
+				updatedAt: current,
+				description: '온보딩을 만들어보자!',
+			},
+			{
+				id: 2,
+				name: '트레이닝 1일차 만들기',
+				done: false,
+				createdAt: current,
+				updatedAt: current,
+				description: '트레이닝 관련 페이지 작성!',
+			},
+		],
+	},
+	reducers: {
+		setChangeInput: (state, action) => {
+			state.key = action.payload.key;
+			state.value = action.payload.value;
 		},
-		{
-			id: 2,
-			name: '트레이닝 1일차 만들기',
-			done: false,
-			createdAt: current,
-			updatedAt: current,
-			description: '트레이닝 관련 페이지 작성!',
+		//
+		setInsert(state, action) {
+			const {
+				id,
+				name,
+				createdAt,
+				updatedAt,
+				description,
+			} = action.payload;
+			state.push({
+				id,
+				name,
+				description,
+				createdAt: current,
+				updatedAt: current,
+				done: false,
+			});
 		},
-	],
-};
+		// setToggle: (state, action) => {
+		// 	state.id = action.payload.id;
+		// },
+		setToggle(state, action) {
+			const todo = state.find((todo) => todo.id === action.payload);
+			if (todo) {
+				todo.done = !todo.done;
+			}
+		},
+		setRemove: (state, action) => {
+			state.id = action.payload.id;
+		},
+		setUpdate: (state, action) => {},
+	},
+});
+
+export const {
+	setChangeInput,
+	setInsert,
+	setToggle,
+	setRemove,
+	setUpdate,
+} = TodoSlice.actions;
+
+export default TodoSlice.reducer;
 
 // const todosSlice = createSlice({
 //     name : 'todos',
@@ -93,38 +139,38 @@ const initialState = {
 //         }
 //     }
 // })
-function todos(state = initialState, action) {
-	switch (action.type) {
-		case CHANGE_INPUT:
-			return {
-				...state,
-				[action.key]: action.value,
-			};
-		case INSERT:
-			return {
-				...state,
-				todos: state.todos.concat(action.todo),
-			};
-		case TOGGLE:
-			return {
-				...state,
-				todos: state.todos.map((todo) =>
-					todo.id === action.id ? {...todo, done: !todo.done} : todo,
-				),
-			};
-		case REMOVE:
-			return {
-				...state,
-				todos: state.todos.filter((todo) => todo.id !== action.id),
-			};
-		case UPDATE:
-			return {
-				...state,
-				name: action.name,
-			};
-		default:
-			return state;
-	}
-}
+// function todos(state = initialState, action) {
+// 	switch (action.type) {
+// 		case CHANGE_INPUT:
+// 			return {
+// 				...state,
+// 				[action.key]: action.value,
+// 			};
+// 		case INSERT:
+// 			return {
+// 				...state,
+// 				todos: state.todos.concat(action.todo),
+// 			};
+// 		case TOGGLE:
+// 			return {
+// 				...state,
+// 				todos: state.todos.map((todo) =>
+// 					todo.id === action.id ? {...todo, done: !todo.done} : todo,
+// 				),
+// 			};
+// 		case REMOVE:
+// 			return {
+// 				...state,
+// 				todos: state.todos.filter((todo) => todo.id !== action.id),
+// 			};
+// 		case UPDATE:
+// 			return {
+// 				...state,
+// 				name: action.name,
+// 			};
+// 		default:
+// 			return state;
+// 	}
+// }
 
-export default todos;
+// export default todos;
