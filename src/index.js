319f6993512185bf7a7f74d634edddfe0a1e4e16
Lee-Gwang-soo <59168432+Lee-Gwang-soo@users.noreply.github.com>
2021-03-12 14:53:53 +0900

    refactor createStore => configureStore

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import App from './App';
 import rootReducer from './modules';
+import {configureStore} from '@reduxjs/toolkit';
 
-const store = createStore(
-	rootReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ &&
-		window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+// const store = createStore(
+// 	rootReducer,
+// window.__REDUX_DEVTOOLS_EXTENSION__ &&
+// 	window.__REDUX_DEVTOOLS_EXTENSION__(),
+// );
+
+const store = configureStore({reducer: rootReducer});
 
 ReactDOM.render(
 	<Provider store={store}>
